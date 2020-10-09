97bebabc55fa617c1d2ac2ac3f7fac9f07efb394
Guillermo Cerezo <wilfas@hotmail.com>
2020-10-09 12:52:46 +0200

    Ejercicios hasta pagina 98

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -27,6 +27,69 @@ namespace CastingConverting
             double g = 9.8;
             int h = ToInt32(g);
             WriteLine($"g is {g} and h is {h}");
+
+            double[] doubles = new[] {9.49, 9.5, 9.51, 10.49, 10.5, 10.51 };
+            foreach(double n in doubles)
+            {
+                WriteLine($"ToInt({n}) is {ToInt32(n)}");
+            }
+
+            foreach(double n in doubles)
+            {
+                WriteLine(format:
+                    "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+                    arg0: n,
+                    arg1: Math.Round(value:n, digits:0, mode:MidpointRounding.AwayFromZero));
+            }
+
+            int number = 12;
+            WriteLine(number.ToString());
+
+            bool boolean = true;
+            WriteLine(boolean.ToString());
+
+            DateTime now = DateTime.Now;
+            WriteLine(now.ToString());
+
+            object me = new object();
+            WriteLine(me.ToString());
+
+            // allocate array of 128 bytes
+            byte[] binaryObject = new byte[128];
+
+            // populate array with random bytes
+            (new Random()).NextBytes(binaryObject);
+
+            WriteLine("Binary Object as bytes:");
+
+            for(int index=0; index < binaryObject.Length; index++)
+            {
+                Write($"{binaryObject[index]:X} ");
+            }
+            WriteLine();
+
+            // convert to Base64 string and output as text
+            string encoded = Convert.ToBase64String(binaryObject);
+            WriteLine($"Binary Object as Base64: {encoded}");
+
+            int age = int.Parse("27");
+            DateTime birthday = DateTime.Parse("4 July 1980");
+
+            WriteLine($"I was born {age} years ago.");
+            WriteLine($"My birthday is {birthday}.");
+            WriteLine($"My birthday is {birthday:D}.");
+
+            Write("How many eggs are there? ");
+            int count;
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out count))
+            {
+                WriteLine($"There are {count} eggs.");
+            }
+            else 
+            {
+                WriteLine("I could not parse the input.");
+            }
         }
     }
 }
