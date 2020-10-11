0e5b0b6939c6ba0a083b4d4731d55d58eddb6663
Guillermo Cerezo <wilfas@hotmail.com>
2020-10-11 16:43:54 +0200

    Termina capítulo 3

---
+++ Chapter03/Exercise04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Write("Enter a number between 0 and 255: ");
+                byte a = byte.Parse(ReadLine());
+
+                Write("Enter another number between 0 and 255: ");
+                byte b = byte.Parse(ReadLine());
+                WriteLine($"{a} divided by {b} is {a / b}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType().Name}: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Chapter03/Exercise05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 3;
+            y = 2 + ++x;
+
+            x = 3 << 2;
+            y = 10 >> 1;
+
+            x = 10 & 8;
+            y = 10 | 7;
+            
+            WriteLine($"x: {x}");
+            WriteLine($"y: {y}");
+        }
+    }
+}
