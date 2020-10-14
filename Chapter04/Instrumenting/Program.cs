b718f6f2d10a582d1ed3b9d6b654de59f76325c8
Guillermo Cerezo <wilfas@hotmail.com>
2020-10-14 10:13:21 +0200

    Añade código hasta la página 133

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+
+            Trace.AutoFlush = true;
+            
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder()
+        }
+    }
+}
