976f5cdee135bc29eebb6a18c0834bc5e1c87e6d
Guillermo Cerezo <wilfas@hotmail.com>
2020-10-15 10:28:31 +0200

    Añade código hasta la página 139

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -16,6 +16,21 @@ namespace Instrumenting
             Trace.WriteLine("Trace says, I am watching!");
 
             var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(
+                displayName: "PacktSwitch",
+                description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
