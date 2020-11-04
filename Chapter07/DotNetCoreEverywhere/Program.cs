1ad74801e91d5ca0d03392cb243ac77c22b5f20c
Guillermo Cerezo <wilfas@hotmail.com>
2020-11-04 09:04:16 +0100

    Añade código hasta página 252

---
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace DotNetCoreEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I can run everywhere!");
+        }
+    }
+}
