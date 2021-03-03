c44668f6cae6ce7f56dfa9c455927e6ef5dea62d
Guillermo Cerezo <wilfas@hotmail.com>
2021-03-03 10:04:43 +0100

    Añade código hasta página 665

---
+++ PracticalApps/NorthwindML/Models/HomeCartViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace NorthwindML.Models
+{
+    public class HomeCartViewModel
+    {
+        public Cart Cart {get;set;}
+
+        public List<EnrichedRecommendation> Recommendations{get;set;}
+    }
+}
